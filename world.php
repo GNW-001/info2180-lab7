be2a755db14de490e218ad629677fece96d060d1
GNW-001 <55669962+GNW-001@users.noreply.github.com>
2019-11-20 21:11:51 -0500

    Change php output to html table format with extra details
    Yeet

--- world.php
+++ world.php
@@ -15,8 +15,20 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>
+  <?php foreach ($results as $row): ?>
+  <tr>
+    <td><?= $row['name']; ?></td>
+    <td><?= $row['continent']; ?></td>
+    <td><?= $row['independence_year']; ?></td>
+    <td><?= $row['head_of_state']; ?></td>
+  </tr>
+  <?php endforeach; ?>
+</table>
